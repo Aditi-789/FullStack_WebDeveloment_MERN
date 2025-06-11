70f8ed14ca754c94ee078f7b6a65c9e0441ddd9c
Aditi Sharma <1508aditisharma@gmail.com>
2025-06-11 16:00:11 +0530

    React JS Day 2

--- React JS/demoshop/src/App.js
+++ React JS/demoshop/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Item';
+import Item from './components/Item';
+import ItemDate from './components/ItemDate';
 
 function App() {
+  const itemtwoName="Ghadi";
   return (
+    <div>
+      <Item name="Nirma">Hello mai hu first item ki name</Item>
+      <ItemDate date="20" month="June" year="1973">Hello mai hu first item ki manufacturing date</ItemDate>
+
+      <Item name={itemtwoName}></Item>
+      <ItemDate date="10" month="July" year="1978"></ItemDate>
+
+      <Item name="Surf Excel"></Item>
+      <ItemDate date="5" month="January" year="1983"></ItemDate>
+      
     <div className="App">
       Hello React
     </div>
+    </div>
+    
   );
 }
 
